d7a1881960c05019ea0baf28ad803901aa5f001b
Nicolai Parlog <nipa@codefx.org>
2019-05-24 18:49:05 +0200

    [06] Create benchmark for URL::equals

---
+++ src/main/java/org/codefx/demo/effective_java/_06_unnecessary_objects/UrlEquals.java
@@ -0,0 +1,28 @@
+package org.codefx.demo.effective_java._06_unnecessary_objects;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UrlEquals extends ObjectCreationBenchmarks {
+
+	private String blog = "https://codefx.org";
+
+	private String youtube = "https://youtube.com/codefx";
+
+	@Benchmark
+	public boolean urlEqual() throws MalformedURLException {
+		// NOTE unnecessary object:
+		//   There isn't anything unnecessary here. It's just that URL::equals
+		//   resolves the host name, which can take a while...
+		return new URL(blog).equals(new URL(youtube));
+	}
+
+	@Benchmark
+	public boolean stringEqual() throws MalformedURLException {
+		return blog.equals(youtube);
+	}
+
+}
